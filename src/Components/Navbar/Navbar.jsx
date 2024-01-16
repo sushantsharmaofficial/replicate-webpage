324f9afd0b321bba01cc04027be380be14516e02
sushanttsharma0011 <135987312+sushanttsharma0011@users.noreply.github.com>
2024-01-16 15:38:20 +0545

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,6 +12,9 @@ export const Navbar = () => {
       id: 1,
       name: "브랜드 이야기",
       active: false,
+      style: "lg:border-r-[1px] border-blue-100  md:h-[630px] md:pr-20",
+      underline: " w-32 lg:w-56",
+      marginSubNav: "",
       sub: [
         {
           id: 4,
@@ -23,6 +26,9 @@ export const Navbar = () => {
       id: 2,
       name: "교육프로그램",
       active: false,
+      style: "lg:border-r-[1px] border-blue-100  md:h-[630px] md:pr-20",
+      underline: " w-32 lg:w-56",
+      marginSubNav: "-ml-40",
       sub: [
         {
           id: 5,
@@ -50,6 +56,9 @@ export const Navbar = () => {
       id: 3,
       name: "THE CLIP 소식",
       active: false,
+      style: "lg:border-r-[1px] border-blue-100  md:h-[630px] md:pr-20",
+      underline: " w-32 lg:w-60",
+      marginSubNav: "ml-3",
       sub: [
         {
           id: 10,
@@ -61,6 +70,9 @@ export const Navbar = () => {
       id: 4,
       name: "고객센터",
       ative: false,
+      style: "",
+      underline: " w-20 lg:w-36",
+      marginSubNav: "-ml-48",
       sub: [
         {
           id: 11,
@@ -99,7 +111,7 @@ export const Navbar = () => {
 
   return (
     <nav className="main-nav shadow-sm   flex relative  hover:bg-white hover:text-black h-10 lg:h-[11vh] px-5 py-7 border-b-[1px]  justify-between items-center text-white ">
-      <div className="logo relative ">
+      <div>
         <img src={logo} className="white-logo   h-7 lg:h-10 " alt="logo" />
         <img src={logo2} className="black-logo   h-7 lg:h-10 " alt="logo" />
 
@@ -130,7 +142,9 @@ export const Navbar = () => {
 
               {element.sub && element.active && (
                 <div className="max-w-[100%]">
-                  <div className=" absolute font-Inter  justify-start pt-12     items-center w-[680px] h-[10vh] p-5.5 lg:flex gap-10">
+                  <div
+                    className={` absolute  font-Inter ${element.marginSubNav}  justify-start pt-12     items-center w-[680px] h-[10vh] p-5.5 lg:flex gap-10`}
+                  >
                     {element.sub.map((sub) => (
                       <a
                         className=" text-black hover:text-blue-700 text-[16px]"
@@ -158,27 +172,29 @@ export const Navbar = () => {
             exit={{ opacity: 0, height: 0 }}
             className="bg-white  z-20 py-5 lg:py-8 absolute w-full overflow-hidden h-[98vh]  top-0 left-0 bottom-0 "
           >
-            <div className=" flex justify-between   lg:mb-36 mb-14 items-center px-5">
-              <div className="logo">
+            <div className=" flex justify-between   lg:mb-36 mb-11 items-center px-5">
+              <div>
                 <img src={logo2} className="h-7 lg:h-10" alt="logo" />
               </div>
               <div onClick={toggleMenu}>
                 <ImCross className=" text-3xl text-black cursor-pointer" />
               </div>
             </div>
-            <ul className=" md:flex md:gap-44  md:px-56  ml-5 px-2 min-w-[680px]  h-screen w-full  gap-5">
+            <ul className=" md:flex md:gap-20  md:px-64  ml-5 px-2 min-w-[680px]   h-screen w-full  gap-5">
               {navElem.map((element) => (
                 <li
-                  className="mb-8 lg:mb-10 leading-[36px] lg:leading-[40px]  lg:block text-[20px]    "
+                  className={`mb-5 lg:mb-10  leading-[36px] lg:leading-[40px]  lg:block text-[20px] ${element.style}`}
                   key={element.id}
                 >
                   <a
-                    className="text-xl   lg:text-4xl font-Inter font-boldest    relative  text-black "
+                    className="   lg:text-[40px] font-bold font-Inter lg:font-boldest    relative  text-black "
                     href="#"
                   >
                     {element.name}
+                    <div
+                      className={` h-[6px] lg:h-[8px] -mt-[14px] lg:-mt-3 bg-[#3AD9C6] ${element.underline}`}
+                    ></div>
                   </a>
-                  <div className=" w-36 lg:w-48 h-[8px] -mt-3 bg-[#37fcf2]"></div>
                   <ul className="mt-1 lg:mt-16">
                     {element.sub.map((sub) => (
                       <li
@@ -186,7 +202,7 @@ export const Navbar = () => {
                         key={sub.id}
                       >
                         <a
-                          className=" hover:text-blue-700 hover:text-[21px] text-[16px] lg:text-[20px]   hover:underline underline-offset-4"
+                          className=" hover:text-blue-700 tracking-tight hover:text-[21px] text-[15px] lg:text-[20px]   hover:underline underline-offset-4"
                           href="#"
                         >
                           {sub.subname}
